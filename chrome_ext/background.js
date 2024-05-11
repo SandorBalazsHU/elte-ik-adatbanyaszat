d104bb1342b1563ee735fa22feb7e19709cd149a
Sándor Balázs <sabuaai@INF.ELTE.HU>
2024-05-11 20:29:26 +0200

    Chrome extension works

--- chrome_ext/background.js
+++ chrome_ext/background.js
@@ -1,15 +1,5 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-      id: "sendToAPI",
-      title: "Send Selection to API",
-      contexts: ["selection"]
-    });
-  });
-  
-  chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId === "sendToAPI") {
-      const selectionText = info.selectionText;
-      fetch('http://127.0.0.1:5000/sum', {
+function process_req(api_url, selectionText) {
+    fetch(api_url, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -18,17 +8,54 @@ chrome.runtime.onInstalled.addListener(function() {
       })
       .then(response => response.json())
       .then(data => {
+        chrome.storage.local.set({result: data});
+        chrome.action.setPopup({popup: "popup.html"});
         console.log(data);
       })
       .catch(error => {
         console.error('Error:', error);
       });
-    }
+}
+
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+      id: "sum",
+      title: "Give summary",
+      contexts: ["selection"]
+    });
+    chrome.contextMenus.create({
+        id: "key",
+        title: "Give keywords",
+        contexts: ["selection"]
+    });
+    chrome.contextMenus.create({
+        id: "title",
+        title: "Give a title",
+        contexts: ["selection"]
+    });
+    chrome.contextMenus.create({
+        id: "length",
+        title: "Count tokens",
+        contexts: ["selection"]
+    });
   });
   
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === "getSelection") {
-      sendResponse({selection: window.getSelection().toString()});
+  chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId === "sum") {
+      const selectionText = info.selectionText;
+      process_req("http://127.0.0.1:5000/sum", selectionText);
+    }
+    if (info.menuItemId === "key") {
+        const selectionText = info.selectionText;
+        process_req("http://127.0.0.1:5000/key", selectionText);
+    }
+    if (info.menuItemId === "title") {
+    const selectionText = info.selectionText;
+    process_req("http://127.0.0.1:5000/title", selectionText);
+    }
+    if (info.menuItemId === "length") {
+    const selectionText = info.selectionText;
+    process_req("http://127.0.0.1:5000/length", selectionText);
     }
   });
-  
--- chrome_ext/popup.js
+++ chrome_ext/popup.js
@@ -1,10 +1,4 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {action: "getSelection"}, function(response) {
-      if (response && response.selection) {
-        document.getElementById("result").innerText = response.selection;
-      } else {
-        document.getElementById("result").innerText = "No text selected.";
-      }
-    });
-  });
-  
+chrome.storage.local.get(['result'], function(result) {
+    //document.getElementById('result').innerText = JSON.parse(result).result.response;
+    document.getElementById('result').innerText = JSON.stringify(result, null, 2);
+});
